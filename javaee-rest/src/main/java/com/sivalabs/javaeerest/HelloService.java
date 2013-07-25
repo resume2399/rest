426f789307be72e58b3d66cc4a33daa63bbcd004
K. Siva Prasad Reddy <sivaprasadreddy.k@gmail.com>
2013-07-25 17:20:06 +0530

    Basic JavaEE6 REST Application setup.

---
+++ javaee-rest/src/main/java/com/sivalabs/javaeerest/HelloService.java
@@ -0,0 +1,24 @@
+package com.sivalabs.javaeerest;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@Path("/hello")
+public class HelloService 
+{
+	private Logger logger = LoggerFactory.getLogger(getClass());
+	
+	@GET
+	@Path("/{name}")
+	@Produces("text/plain")
+    public String sayHello(@PathParam("name") String name)
+    {
+        logger.debug("Name : {}", name);
+    	return "Hello "+name;
+    }
+}
---
+++ javaee-rest/src/main/java/com/sivalabs/javaeerest/RestConfig.java
@@ -0,0 +1,23 @@
+package com.sivalabs.javaeerest;
+
+/*import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;*/
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ * @author Siva
+ *
+ */
+@ApplicationPath("/rest")
+public class RestConfig extends Application
+{
+	/*@SuppressWarnings("unchecked")
+	@Override
+	public Set<Class<?>> getClasses()
+	{
+		return new HashSet<Class<?>>(Arrays.asList(HelloService.class));
+	}*/
+}
